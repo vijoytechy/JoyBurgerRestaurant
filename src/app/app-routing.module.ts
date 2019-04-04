785205387b4b352ef172da7e2456f0eba9f7c848
Vijoy T Abraham <vijoytechy@gmail.com>
2019-04-04 19:59:21 +0530

    Joy Burger Website in Angular 7 Initial release

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ServicesComponent } from './components/services/services.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { OffersComponent } from './components/offers/offers.component';
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+}, {
 
-const routes: Routes = [];
+  path: 'home',
+  component: HomeComponent
+},
+{
+  path: 'about',
+  component: AboutComponent
+},
+{
+  path: 'menu',
+  component: MenuComponent
+},
+{
+  path: 'services',
+  component: ServicesComponent
+},
+{
+  path: 'gallery',
+  component: GalleryComponent
+},
+{
+  path: 'testimonials',
+  component: TestimonialsComponent
+}, {
+  path: 'contact',
+  component: ContactComponent
+}, {
+  path: 'offers',
+  component: OffersComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavComponent } from './components/nav/nav.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ServicesComponent } from './components/services/services.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { OffersComponent } from './components/offers/offers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NavComponent,
+    MenuComponent,
+    ServicesComponent,
+    GalleryComponent,
+    TestimonialsComponent,
+    ContactComponent,
+    FooterComponent,
+    OffersComponent
   ],
   imports: [
     BrowserModule,
